a498665433af12dc6ba614ca5408a6dce529986b
HanGao42 <zcemhg0@ucl.ac.uk>
2025-01-23 17:44:59 +0000

    Week1 finfished!!!

--- src/student/dash_single/paralympics_dash.py
+++ src/student/dash_single/paralympics_dash.py
@@ -42,12 +42,117 @@ div_three = html.Div(
     className="border border-dark rounded p-4 mb-4"
 )
 
+# row_one = dbc.Row(
+#     [
+#         dbc.Col(
+#             html.Div([
+#                 html.H1("Paralympics Data Analytics"),
+#                 html.P("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent congue luctus elit nec gravida.")
+#             ])
+#         )
+#     ],
+#     className="border border-dark rounded p-4 mb-4"
+# )
+
+row_one = dbc.Row(
+    [
+        dbc.Col(
+            children=[
+                html.H1("Paralympics Data Analytics"),
+                html.P("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent congue luctus elit nec gravida.")
+            ], width=12,
+        )
+    ], #className="border border-dark rounded p-4 mb-4"
+)
+
+row_two = dbc.Row(
+    [
+        dbc.Col(
+            children=[
+                dbc.Select(
+                    options=[
+                        {"label": "Events", "value": "events"},  # The value is in the format of the column heading in the data
+                        {"label": "Sports", "value": "sports"},
+                        {"label": "Countries", "value": "countries"},
+                        {"label": "Athletes", "value": "participants"},
+                    ],
+                    value="events",  # The default selection
+                    id="dropdown-input",  # id uniquely identifies the element, will be needed later for callbacks
+                ),
+            ], width=4, #className="border border-dark rounded p-4 mb-4"
+        ),
+
+        dbc.Col(
+            children=[
+                html.Div(
+                    [
+                        dbc.Label("Select the Paralympic Games type"),
+                        dbc.Checklist(
+                            options=[
+                                {"label": "Summer", "value": "summer"},
+                                {"label": "Winter", "value": "winter"},
+                            ],
+                            value=["summer"],  # Values is a list as you can select 1 AND 2
+                            id="checklist-input",
+                        ),
+                    ]
+                )
+            ], width={"size": 4, "offset": 2}, #className="border border-dark rounded p-4 mb-4"
+        )
+    ]
+)
+
+row_three = dbc.Row(
+    [
+        dbc.Col(
+            children=[
+                html.Img(src=app.get_asset_url('line-chart-placeholder.png'), className="img-fluid")
+            ], width=6, #className="border border-dark rounded p-4 mb-4"
+        ),
+
+        dbc.Col(
+            children=[
+                html.Img(src=app.get_asset_url('bar-chart-placeholder.png'), className="img-fluid")
+            ], width=6, #className="border border-dark rounded p-4 mb-4"
+        )
+    ]
+)
+
+row_four = dbc.Row(
+    [
+        dbc.Col(
+            children=[
+                html.Img(src=app.get_asset_url('map-placeholder.png'), className="img-fluid")
+            ], width=8, #className="border border-dark rounded p-4 mb-4"
+        ),
+
+        dbc.Col(
+            children=[
+                dbc.Card(
+                    [
+                        dbc.CardImg(src=app.get_asset_url("logos/2022_Beijing.jpg"), top=True),
+                        dbc.CardBody(
+                            [
+                                html.H4("Beijing 2022", className="card-title"),
+                                html.P("Number of athletes: XX", className="card-text", ),
+                                html.P("Number of events: XX", className="card-text", ),
+                                html.P("Number of countries: XX", className="card-text", ),
+                                html.P("Number of sports: XX", className="card-text", ),
+                            ]
+                        )
+                    ], style={"width": "18rem"},
+                ), 
+            ], width=4, #className="border border-dark rounded p-4 mb-4"
+        )
+    ]
+)
+
 app.layout = dbc.Container([
-    html.H1("Paralympics Data Analytics", className="border border-dark rounded p-4 mb-4"),
-    html.P("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent congue luctus elit nec gravida.", className="border border-dark rounded p-4 mb-4"),
-    div_one,
-    div_two,
-    div_three
+    row_one,
+    row_two,
+    row_three,
+    row_four
+
 ])
 
 # Run the app
