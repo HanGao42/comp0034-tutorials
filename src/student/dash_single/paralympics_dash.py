3d8d34daa8c4c8543832a240e6e93eb7833a5b14
HanGao42 <zcemhg0@ucl.ac.uk>
2025-01-23 02:06:49 +0000

    Roughly understand

--- src/student/dash_single/paralympics_dash.py
+++ src/student/dash_single/paralympics_dash.py
@@ -1,13 +1,53 @@
-# Imports for Dash and Dash.html
 from dash import Dash, html
+import dash_bootstrap_components as dbc
 
-# Create an instance of the Dash app
-app = Dash(__name__)
+# Variable that defines the meta tag for the viewport
+meta_tags = [
+    {"name": "viewport", "content": "width=device-width, initial-scale=1"},
+]
 
-# Add an HTML layout to the Dash app
-app.layout = html.Div([
-    # Add an HTML div with the text 'Hello World'
-    html.Div(children='Hello World')
+# Variable that contains the external_stylesheet to use, in this case Bootstrap styling from dash bootstrap components (dbc)
+external_stylesheets = [dbc.themes.BOOTSTRAP]
+
+# Pass the stylesheet and meta_tag variables to the Dash app constructor
+app = Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=meta_tags)
+
+# Wrap the layout in a Bootstrap container
+div_one = html.Div(
+    [
+        dbc.Row([
+            dbc.Col(['Divivion 1']),
+        ])
+    ],
+    className="border border-dark rounded p-4 mb-4"
+)
+
+div_two = html.Div(
+    [
+        dbc.Row([
+            dbc.Col(['Col 1'], className="border border-dark rounded p-4 mb-4"),
+            dbc.Col(['Col 2'], className="border border-dark rounded p-4 mb-4"),
+            dbc.Col(['Col 3'], className="border border-dark rounded p-4 mb-4"),
+        ])
+    ],
+    className="border border-dark rounded p-4 mb-4"
+)
+
+div_three = html.Div(
+    [
+        dbc.Row([
+            dbc.Col(['S 4 O 5'], width={"size": 4, "offset": 5}),
+        ])
+    ],
+    className="border border-dark rounded p-4 mb-4"
+)
+
+app.layout = dbc.Container([
+    html.H1("Paralympics Data Analytics", className="border border-dark rounded p-4 mb-4"),
+    html.P("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent congue luctus elit nec gravida.", className="border border-dark rounded p-4 mb-4"),
+    div_one,
+    div_two,
+    div_three
 ])
 
 # Run the app
