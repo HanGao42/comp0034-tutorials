04f3cbe933a8459c918dc82f71a453eaeaa22f93
HanGao42 <zcemhg0@ucl.ac.uk>
2025-04-02 23:13:01 +0100

    redo week6

--- src/student/flask_paralympics/__init__.py
+++ src/student/flask_paralympics/__init__.py
@@ -1,29 +1,24 @@
-# flask_paralympics/__init__.py
-
 import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-
+    
     app.config.from_mapping(
         SECRET_KEY='dev',
-        SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(app.instance_path, 'paralympics.sqlite'),
-        SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
 
-    db.init_app(app)
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
 
-    # 注册路由
-    with app.app_context():
-        from student.flask_paralympics.routes import main
-        app.register_blueprint(main)
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-        # 导入模型并创建表
-        from student.flask_paralympics import models
-        db.create_all()
+    from student.flask_paralympics.routes import main
+    app.register_blueprint(main)
 
     return app
--- src/student/flask_paralympics/routes.py
+++ src/student/flask_paralympics/routes.py
@@ -1,5 +1,3 @@
-# flask_paralympics/routes.py
-
 from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
@@ -10,13 +8,12 @@ def index():
 
 @main.route('/hello/<name>')
 def hello(name):
-    return f"你好 {name}，欢迎来到 Paralympics App!"
-
-from flask import request
+    return render_template('hello.html', name=name)
 
 @main.route('/welcome', methods=['GET', 'POST'])
 def welcome():
+    from flask import request, redirect, url_for, session
     if request.method == 'POST':
         name = request.form['username']
-        return render_template('hello.html', name=name)
+        return redirect(url_for('main.hello', name=name))
     return render_template('form.html')
